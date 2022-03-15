0fe0b009cf2b98c76b18a5a3e23ae36242613aa8
Varun <lanjharav@gmail.com>
2022-03-15 18:42:54 +0530

    Navbar stuff done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect } from "react";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Tooltip,
+  useColorMode,
+  IconButton,
+  AvatarBadge,
+} from "@chakra-ui/react";
+import { ImSearch } from "react-icons/im";
+import { FaSun, FaMoon, FaUser } from "react-icons/fa";
+import { IoMdCreate } from "react-icons/io";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Flex
+      justifyContent="space-between"
+      alignItems="center"
+      p={4}
+      position="sticky"
+      top={0}
+      zIndex="100"
+      backgroundColor={colorMode === "light" ? "#ffffff" : "#1a202c"}
+    >
+      <Flex>
+        <Heading
+          color={colorMode === "light" ? "black" : "white"}
+          size="xl"
+          fontFamily="Sansita Swashed"
+        >
+          OnlyUwU
+        </Heading>
+      </Flex>
+      <Tooltip label="Search shit" openDelay={400}>
+        <InputGroup mx={8} width="50vw">
+          <InputLeftElement
+            pointerEvents="none"
+            children={<ImSearch color="gray.300" />}
+          />
+          <Input type="text" placeholder="Search shit ..." variant="filled" />
+        </InputGroup>
+      </Tooltip>
+      {colorMode === "light" ? (
+        <Tooltip label="Dark mode" openDelay={400}>
+          <IconButton
+            icon={<FaMoon />}
+            aria-label="Dark mode"
+            onClick={() => {
+              toggleColorMode();
+            }}
+          />
+        </Tooltip>
+      ) : (
+        <Tooltip label="Light mode" openDelay={400}>
+          <IconButton
+            icon={<FaSun />}
+            aria-label="Light mode"
+            onClick={() => {
+              toggleColorMode();
+            }}
+          />
+        </Tooltip>
+      )}
+      <Tooltip label="Create shit" openDelay={400}>
+        <Button
+          leftIcon={<IoMdCreate />}
+          colorScheme="purple"
+          variant="solid"
+          borderRadius={24}
+          padding={6}
+        >
+          Create
+        </Button>
+      </Tooltip>
+      <Tooltip label="Idiot" openDelay={400}>
+        <Avatar cursor="pointer" />
+      </Tooltip>
+    </Flex>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Grid } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    document.title = "OnlyUwU";
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <Grid
+        gridTemplateColumns="20vw auto 24vw"
+        columnGap="2rem"
+        marginLeft="1rem"
+        marginRight="1rem"
+      >
+        <p>Loll</p>
+        <p>Lma</p>
+        <p>Lmaooo</p>
+      </Grid>
+    </div>
+  );
 };
 
 export default Home;
